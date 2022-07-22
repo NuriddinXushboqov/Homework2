99144428724512121145d56676862ca2c1bc286e
NuriddinBro <nuriddinxushboqov03gmail.com>
2022-07-22 11:43:28 +0500

    commit devo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Nuriddin Xushboqov");
+        System.out.println("O`zgarish boldi");
     }
 }
